acef085054b41a5659d7aa4b805f8fdd7f5ce534
Lucas Weaver <l-weaver@ti.com>
2021-09-01 16:43:29 -0500

    [ADASVISION-4957] Migrating Lidar kernels to use portable DSP API

--- kernels/lidar/arm/vx_lidar_gpc_target.c
+++ kernels/lidar/arm/vx_lidar_gpc_target.c
@@ -377,18 +377,6 @@ void tivxAddTargetKernelLidarGpc(void)
 
     self_cpu = tivxGetSelfCpuId();
 
-    if ( self_cpu == TIVX_CPU_ID_DSP1 )
-    {
-        strncpy(target_name[0], TIVX_TARGET_DSP1, TIVX_TARGET_MAX_NAME);
-        status = VX_SUCCESS;
-    }
-    else
-    if ( self_cpu == TIVX_CPU_ID_DSP2 )
-    {
-        strncpy(target_name[0], TIVX_TARGET_DSP2, TIVX_TARGET_MAX_NAME);
-        status = VX_SUCCESS;
-    }
-    else
     if ( self_cpu == TIVX_CPU_ID_A72_0 )
     {
         strncpy(target_name[0], TIVX_TARGET_A72_0, TIVX_TARGET_MAX_NAME);
@@ -406,7 +394,7 @@ void tivxAddTargetKernelLidarGpc(void)
     }
     else
     {
-        status = VX_FAILURE;
+        status = tivxKernelsTargetUtilsAssignTargetNameDsp(target_name[0]);
     }
 
     if (status == VX_SUCCESS)
