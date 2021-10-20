c81473b8abd95a508ebd1204b8782e608d0ab52b
Jesse Villarreal <jesse.villarreal@ti.com>
2021-10-20 09:51:22 -0500

    [ADASVISION-5041] Fixing improper header

--- kernels/srv/c66/lens_distortion_correction.c
+++ kernels/srv/c66/lens_distortion_correction.c
@@ -1,25 +1,64 @@
-/*=======================================================================
-*
-*            Texas Instruments Internal Reference Software
-*
-*                           EP Systems Lab
-*                    Perception & Analytics Lab R&D
-*
-*         Copyright (c) 2015 Texas Instruments, Incorporated.
-*                        All Rights Reserved.
-*
-*
-*          FOR TI INTERNAL USE ONLY. NOT TO BE REDISTRIBUTED.
-*
-*                 TI Confidential - Maximum Restrictions
-*
-*
-*
-*=======================================================================
-*
-*  File: lens_distortion_correction.c
-*
-=======================================================================*/
+/*
+ *
+ * Copyright (c) 2021 Texas Instruments Incorporated
+ *
+ * All rights reserved not granted herein.
+ *
+ * Limited License.
+ *
+ * Texas Instruments Incorporated grants a world-wide, royalty-free, non-exclusive
+ * license under copyrights and patents it now or hereafter owns or controls to make,
+ * have made, use, import, offer to sell and sell ("Utilize") this software subject to the
+ * terms herein.  With respect to the foregoing patent license, such license is granted
+ * solely to the extent that any such patent is necessary to Utilize the software alone.
+ * The patent license shall not apply to any combinations which include this software,
+ * other than combinations with devices manufactured by or for TI ("TI Devices").
+ * No hardware patent is licensed hereunder.
+ *
+ * Redistributions must preserve existing copyright notices and reproduce this license
+ * (including the above copyright notice and the disclaimer and (if applicable) source
+ * code license limitations below) in the documentation and/or other materials provided
+ * with the distribution
+ *
+ * Redistribution and use in binary form, without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * *       No reverse engineering, decompilation, or disassembly of this software is
+ * permitted with respect to any software provided in binary form.
+ *
+ * *       any redistribution and use are licensed by TI for use only with TI Devices.
+ *
+ * *       Nothing shall obligate TI to provide you with source code for the software
+ * licensed and provided to you in object code.
+ *
+ * If software source code is provided to you, modification and redistribution of the
+ * source code are permitted provided that the following conditions are met:
+ *
+ * *       any redistribution and use of the source code, including any resulting derivative
+ * works, are licensed by TI for use only with TI Devices.
+ *
+ * *       any redistribution and use of any object code compiled from the source code
+ * and any resulting derivative works, are licensed by TI for use only with TI Devices.
+ *
+ * Neither the name of Texas Instruments Incorporated nor the names of its suppliers
+ *
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * DISCLAIMER.
+ *
+ * THIS SOFTWARE IS PROVIDED BY TI AND TI'S LICENSORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL TI AND TI'S LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 #include "TI/tivx_srv.h"
 #include "lens_distortion_correction.h"
@@ -49,7 +88,7 @@
 
 #if LDC_LIB_U2D_LUT_TYPE == 0 || LDC_LIB_U2D_LUT_TYPE == 2
         dtype ru;
-        #if LDC_LIB_DATA_TYPE==0 
+        #if LDC_LIB_DATA_TYPE==0
                 ru = sqrtf(diffX*diffX + diffY*diffY);
                 lut_in_val = atanf(ru*ldc->distFocalLengthInv);
         #elif LDC_LIB_DATA_TYPE==1
@@ -57,7 +96,7 @@
                 lut_in_val = atan(ru*ldc->distFocalLengthInv);
         #endif
 #elif LDC_LIB_U2D_LUT_TYPE == 1
-        #if LDC_LIB_DATA_TYPE==0 
+        #if LDC_LIB_DATA_TYPE==0
                 lut_in_val = sqrtf(diffX*diffX + diffY*diffY);
         #elif LDC_LIB_DATA_TYPE==1
                 lut_in_val = sqrt(diffX*diffX + diffY*diffY);
@@ -86,7 +125,7 @@
 }
 
 
-#if LDC_LIB_DATA_TYPE<2 
+#if LDC_LIB_DATA_TYPE<2
  dtype lut_lookup_floating(dtype *lut, dtype inval, int32_t indMax, dtype stepInv, LDC_status *status)
 {
         *status = LDC_STATUS_OK;
