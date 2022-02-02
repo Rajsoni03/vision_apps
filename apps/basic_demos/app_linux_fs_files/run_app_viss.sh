d29ad555db0b1ae870c203f16eb0fca12e5083bd
Shubham <s-gore@ti.com>
2022-02-02 16:35:06 -0600

    ADASVISION-5147 Added run_app_viss.sh to run comprehensive stress test of the VISS app.

---
+++ apps/basic_demos/app_linux_fs_files/run_app_viss.sh
@@ -0,0 +1,29 @@
+echo "!Starting VISS APP Stress Test!"
+/opt/vision_apps/vx_app_viss.out 5 60
+echo "!10 Second Cooldown!"
+/opt/vision_apps/vx_app_viss.out 15 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_viss.out 25 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_viss.out 35 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_viss.out 45 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_viss.out 55 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_viss.out 65 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_viss.out 75 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_viss.out 85 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_viss.out 95 60
+echo "!Finished VISS APP Stress Test!"
