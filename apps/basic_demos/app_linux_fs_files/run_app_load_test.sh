cf5837f92358f3be86343002ce9977f0c0e75474
Shubham <s-gore@ti.com>
2022-02-03 13:36:43 -0600

    ADASVISION-5148 Added run_app_load_test.sh to run comprehensive stress test of the App Load Test .

---
+++ apps/basic_demos/app_linux_fs_files/run_app_load_test.sh
@@ -0,0 +1,94 @@
+echo "!Starting Load Test!"
+
+echo "!Starting C7x Core Testing!"
+/opt/vision_apps/vx_app_load_test.out 2 5 60
+echo "!10 Second Cooldown!"
+/opt/vision_apps/vx_app_load_test.out 2 15 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 2 25 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 2 35 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 2 45 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 2 55 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 2 65 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 2 75 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 2 85 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 2 95 60
+echo "!10 Second Cooldown!"
+sleep 10
+
+echo "!Starting R5F Core Testing!"
+/opt/vision_apps/vx_app_load_test.out 4 5 60
+echo "!10 Second Cooldown!"
+/opt/vision_apps/vx_app_load_test.out 4 15 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 4 25 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 4 35 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 4 45 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 4 55 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 4 65 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 4 75 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 4 85 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 4 95 60
+echo "!10 Second Cooldown!"
+sleep 10
+
+echo "!Starting C6x Core Testing!"
+/opt/vision_apps/vx_app_load_test.out 5 5 60
+echo "!10 Second Cooldown!"
+/opt/vision_apps/vx_app_load_test.out 5 15 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 5 25 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 5 35 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 5 45 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 5 55 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 5 65 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 5 75 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 5 85 60
+echo "!10 Second Cooldown!"
+sleep 10
+/opt/vision_apps/vx_app_load_test.out 5 95 60
+
+echo "!Finished Load Test!"
