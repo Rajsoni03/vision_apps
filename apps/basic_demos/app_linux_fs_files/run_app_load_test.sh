e63996c8f6529c09dc6d1303a180ebe3e6553abb
Shubham Gore <s-gore@ti.com>
2022-02-09 14:15:03 -0600

    Reduced tests to 3 iterations and reduced duration to 5 secs each

--- apps/basic_demos/app_linux_fs_files/run_app_load_test.sh
+++ apps/basic_demos/app_linux_fs_files/run_app_load_test.sh
@@ -1,94 +1,31 @@
 echo "!Starting App Load Test!"
 
-/opt/vision_apps/vx_app_load_test.out 2 5 60
+/opt/vision_apps/vx_app_load_test.out 2 0 5
 echo "!10 Second Cooldown!"
 sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 15 60
+/opt/vision_apps/vx_app_load_test.out 2 50 5
 echo "!10 Second Cooldown!"
 sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 25 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 35 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 45 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 55 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 65 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 75 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 85 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 2 95 60
+/opt/vision_apps/vx_app_load_test.out 2 100 5
 echo "!10 Second Cooldown!"
 sleep 10
 
-/opt/vision_apps/vx_app_load_test.out 4 5 60
+/opt/vision_apps/vx_app_load_test.out 4 0 5
 echo "!10 Second Cooldown!"
 sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 15 60
+/opt/vision_apps/vx_app_load_test.out 4 50 5
 echo "!10 Second Cooldown!"
 sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 25 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 35 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 45 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 55 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 65 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 75 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 85 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 4 95 60
+/opt/vision_apps/vx_app_load_test.out 4 100 5
 echo "!10 Second Cooldown!"
 sleep 10
 
-/opt/vision_apps/vx_app_load_test.out 5 5 60
+/opt/vision_apps/vx_app_load_test.out 5 0 5
 echo "!10 Second Cooldown!"
 sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 15 60
+/opt/vision_apps/vx_app_load_test.out 5 50 5
 echo "!10 Second Cooldown!"
 sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 25 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 35 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 45 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 55 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 65 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 75 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 85 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_load_test.out 5 95 60
+/opt/vision_apps/vx_app_load_test.out 5 100 5
 
 echo "!Finished Load Test!"
