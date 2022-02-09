d506c7b99155a007de07e92ee460621ed5345c74
Shubham Gore <s-gore@ti.com>
2022-02-09 14:11:51 -0600

    Reduced tests to 3 iterations and reduced duration to 5 secs each

--- apps/basic_demos/app_linux_fs_files/run_app_viss.sh
+++ apps/basic_demos/app_linux_fs_files/run_app_viss.sh
@@ -1,30 +1,9 @@
 echo "!Starting VISS APP Stress Test!"
-/opt/vision_apps/vx_app_viss.out 5 60
+/opt/vision_apps/vx_app_viss.out 0 5
 echo "!10 Second Cooldown!"
 sleep 10
-/opt/vision_apps/vx_app_viss.out 15 60
+/opt/vision_apps/vx_app_viss.out 50 5
 echo "!10 Second Cooldown!"
 sleep 10
-/opt/vision_apps/vx_app_viss.out 25 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_viss.out 35 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_viss.out 45 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_viss.out 55 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_viss.out 65 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_viss.out 75 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_viss.out 85 60
-echo "!10 Second Cooldown!"
-sleep 10
-/opt/vision_apps/vx_app_viss.out 95 60
+/opt/vision_apps/vx_app_viss.out 100 5
 echo "!Finished VISS APP Stress Test!"
