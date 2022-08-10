125d04ce322df20dd380ebaee605f85f6a2a8c51
Jesse Villarreal <jesse.villarreal@ti.com>
2022-08-10 15:51:28 -0500

    Adding build time to make_sdk.sh script

--- make_sdk.sh
+++ make_sdk.sh
@@ -1,16 +1,17 @@
 NUM_PROCS=$(cat /proc/cpuinfo | grep processor | wc -l)
-make sdk -j${NUM_PROCS} 2> >(tee /tmp/sdk_build_errors.log)
+log_file="/tmp/sdk_build_errors.log"
+time make sdk -j${NUM_PROCS} 2> >(tee ${log_file})
 if [ $? -ne 0 ]
 then
-	echo ""
-	echo "###################################################"
-	echo "BUILD ERRORS:"
-	echo "###################################################"
-	echo ""
-	cat /tmp/sdk_build_errors.log
-	echo ""
-	echo "###################################################"
-	echo "Error log file also here: /tmp/sdk_build_errors.log"
-	echo "###################################################"
-	echo ""
+    echo ""
+    echo "###################################################"
+    echo "BUILD ERRORS:"
+    echo "###################################################"
+    echo ""
+    cat ${log_file}
+    echo ""
+    echo "###################################################"
+    echo "Error log file also here: ${log_file}"
+    echo "###################################################"
+    echo ""
 fi
