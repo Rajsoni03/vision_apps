e898a3bccc1e20074df5e206365b46a59ee19146
Shreyash Sinha <s-sinha@ti.com>
2025-11-24 22:44:48 +0530

    platform: j721s2: lpm: disable dmpac and ipc according to ecu build flag
    - Disabling H/W IPs and IPC as per the ECU flags defined in sdk builder
    
    Signed-off-by: Rajkumar Soni <r-soni@ti.com>
    Signed-off-by: Shreyash Sinha <s-sinha@ti.com>

--- platform/j721s2/rtos/common/app_cfg.h
+++ platform/j721s2/rtos/common/app_cfg.h
@@ -76,8 +76,13 @@
 #define ENABLE_IPC_MCU2_1
 //#define ENABLE_IPC_MCU3_0
 //#define ENABLE_IPC_MCU3_1
-#define ENABLE_IPC_C7x_1
-#define ENABLE_IPC_C7x_2
+
+#if (defined(ECU_FC))                                         // Enabling C7x_1 IPC only for FC Demo
+    #define ENABLE_IPC_C7x_1
+#else                                                         // Enabling all C7x IPCs for SRV, AVP4 or non ECU demos
+    #define ENABLE_IPC_C7x_1
+    #define ENABLE_IPC_C7x_2
+#endif
 
 #define ENABLE_UDMA
 #define ENABLE_UDMA_COPY
